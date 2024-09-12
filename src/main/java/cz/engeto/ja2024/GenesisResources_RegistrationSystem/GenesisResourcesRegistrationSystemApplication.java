d3387199fbaccb3c592e9b013b126d37a878a291
P3ANTMC <pean.tmc@seznam.cz>
2024-09-12 20:46:07 +0200

    Initial commit

---
+++ src/main/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/GenesisResourcesRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package cz.engeto.ja2024.GenesisResources_RegistrationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisResourcesRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesisResourcesRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/GenesisResourcesRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.engeto.ja2024.GenesisResources_RegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisResourcesRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

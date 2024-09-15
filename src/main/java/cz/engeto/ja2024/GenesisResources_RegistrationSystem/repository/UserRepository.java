4102621c0f04c76a01cd36acfc6a23ce94623ba1
P3ANTMC <pean.tmc@seznam.cz>
2024-09-15 16:19:21 +0200

    UserRepository class created.
    It's used for manipulation of user data in the database.

---
+++ src/main/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package cz.engeto.ja2024.GenesisResources_RegistrationSystem.repository;
+
+import cz.engeto.ja2024.GenesisResources_RegistrationSystem.databaseconfiguration.DatabaseConfiguration;
+import cz.engeto.ja2024.GenesisResources_RegistrationSystem.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+
+@Repository
+public class UserRepository {
+
+    private final DatabaseConfiguration databaseConfiguration;
+
+    public UserRepository(@Autowired final DatabaseConfiguration databaseConfiguration) {
+        this.databaseConfiguration = databaseConfiguration;
+    }
+
+}
+
+
+

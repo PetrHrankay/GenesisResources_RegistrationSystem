9056449ad0cc22888cdaf78bedf45961223de923
P3ANTMC <pean.tmc@seznam.cz>
2024-09-16 21:49:47 +0200

    Class was moved to utils package.

--- src/main/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/config/Settings.java
+++
@@ -1,9 +0,0 @@
-package cz.engeto.ja2024.GenesisResources_RegistrationSystem.config;
-
-public class Settings {
-
-    public static final String PERSONIDFILENAME = "resources/dataPersonId.txt";
-
-
-
-}

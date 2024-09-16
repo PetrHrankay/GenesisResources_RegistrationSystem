c85a43dcd2fe21976fef82e0b8d5899d61b35caa
P3ANTMC <pean.tmc@seznam.cz>
2024-09-16 20:54:51 +0200

    Settings class created.
    The class is used to store the file path for simplification and improved code maintainability.

---
+++ src/main/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/config/Settings.java
@@ -0,0 +1,9 @@
+package cz.engeto.ja2024.GenesisResources_RegistrationSystem.config;
+
+public class Settings {
+
+    public static final String PERSONIDFILENAME = "resources/dataPersonId.txt";
+
+
+
+}

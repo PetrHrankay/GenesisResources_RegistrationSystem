8d5e3c701eece0c3d67422ab177c927b470042de
P3ANTMC <pean.tmc@seznam.cz>
2024-09-24 15:07:36 +0200

    getAllUsers method added.
    The method returns all users un database.

--- src/main/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/service/UserService.java
+++ src/main/java/cz/engeto/ja2024/GenesisResources_RegistrationSystem/service/UserService.java
@@ -124,5 +124,27 @@ public class UserService {
         }
     }
 
+    public List<User> getAllUsers() throws SQLException {
+        String query = "SELECT id, name, surname FROM users";
+
+        List<User> out = new ArrayList<>();
+
+        try (Connection connection = databaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                User user = new User(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("surname")
+                );
+                out.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return out;
+    }
 
 }
